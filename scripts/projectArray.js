b2c728b8174cd3a9ce1bc6e192c850242a7349c0
nsmeds <niilosmeds@gmail.com>
2016-09-20 16:40:35 -0700

    Minor organizing and cleanup of code.

--- scripts/projectArray.js
+++ scripts/projectArray.js
@@ -2,16 +2,16 @@ var projectArray = [
   {
     title: 'First Project',
     body: '<p>Here is where a description of this project will live! I am confident that it will be impressive visually and conceptually. This section may contain a detailed summary, a screenshot and a link to the GitHub repo for this particular project. Let us marvel at the unparalleled glory of this particular project, and let us consider offering its creator a high-paying position with generous perks and benefits.</p>',
-    date: '2016-09-20'
+    date: '2016-09-17'
   },
   {
     title: 'Second Project',
     body: '<p>Here is where a description of my second project will live! I am confident that it will be impressive visually and conceptually. This section may contain a detailed summary, a screenshot and a link to the GitHub repo for this particular project. Let us marvel at the unparalleled glory of this particular project, and let us consider offering its creator a high-paying position with generous perks and benefits.</p>',
-    date: '2016-09-19'
+    date: '2016-09-13'
   },
   {
     title: 'Third Project',
     body: '<p>Here is where a description of yet another project will live! I am confident that it will be impressive visually and conceptually. This section may contain a detailed summary, a screenshot and a link to the GitHub repo for this particular project. Let us marvel at the unparalleled glory of this particular project, and let us consider offering its creator a high-paying position with generous perks and benefits.</p>',
-    date: '2016-09-18'
+    date: '2016-09-10'
   },
 ];
