695307c49c1a0b9a968770feecd3debb78c823d1
nsmeds <niilosmeds@gmail.com>
2016-09-21 17:06:45 -0700

    Project descriptions truncated initially, then shown when user clicks 'more'

--- scripts/sectionView.js
+++ scripts/sectionView.js
@@ -1,6 +1,7 @@
 var sectionView = {};
 
-sectionView.tabToggle = function () {
+// Show only the content currently selected via tab
+sectionView.tabToggle = function() {
   $('.menu-items').on('click', '.tab', function() {
     $('.tab-content').hide();
     var $selectedTab = $(this).attr('data-content');
@@ -9,4 +10,17 @@ sectionView.tabToggle = function () {
   $('.menu-items .tab:first').click();
 };
 
+// Show only the first paragraph of each project description, then reveal hidden paragraphs when 'more' is clicked.
+sectionView.showMore = function() {
+  $('.project *:nth-of-type(n+2)').hide();
+  $('a.read-more').on('click', function(e) {
+    e.preventDefault();
+    var $selectedProject = $(this).prev();
+    $selectedProject.find('p').show();
+    $(this).hide();
+  });
+};
+
+// Invoke the functions defined above.
 sectionView.tabToggle();
+sectionView.showMore();
