f974f01a8101f0dd86f5151fe61e1ac4c629c48c
nsmeds <niilosmeds@gmail.com>
2016-09-26 15:27:14 -0700

    Created data folder and json file for project data

--- scripts/projects.js
+++ scripts/projects.js
@@ -1,11 +1,11 @@
-var projects = [];
-
 // Create a constructor function for project data.
 function Project (opts) {
   for (var key in opts) {
     this[key] = opts[key];
   }
 }
+
+Project.all = [];
 
 // Create a handlebars template and populate the project descriptions
 Project.prototype.toHtml = function() {
@@ -17,15 +17,22 @@ Project.prototype.toHtml = function() {
 };
 
 // Sort projects by date, with the most recently published projects at the top.
-projectArray.sort(function(curElem, nextElem) {
-  return (new Date(nextElem.date)) - (new Date(curElem.date));
-});
+Project.loadAll = function(data) {
+  data.sort(function(a,b) {
+    return (new Date(nextElem.date)) - (new Date(curElem.date));
+  }).forEach(function(ele) {
+    Project.all.push(new Project(ele));
+  });
+};
 
-// Loop through projects and append each to the DOM.
-projectArray.forEach(function(ele) {
-  projects.push(new Project(ele));
-});
+// ...
 
-projects.forEach(function(a) {
-  $('#projects').append(a.toHtml());
-});
+Project.fetchAll = function() {
+  if (localStorage.projects) {
+    Project.loadAll(JSON.parse(localStorage.getItem(projects)));
+    sectionView.
+  } else {
+
+  }
+
+};
