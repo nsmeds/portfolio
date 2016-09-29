327cd6284c6e2592585e535a9c2759458b1705e3
nsmeds <niilosmeds@gmail.com>
2016-09-29 14:28:56 -0700

    Wrapped each script file in an IIFE and exposed it to the window, rewriting function references where appropriate.

--- scripts/projects.js
+++ scripts/projects.js
@@ -1,52 +1,56 @@
 // Create a constructor function for project data.
-function Project (opts) {
-  for (var key in opts) {
-    this[key] = opts[key];
+(function(module) {
+
+  function Project (opts) {
+    for (var key in opts) {
+      this[key] = opts[key];
+    }
   }
-}
 
-Project.all = [];
+  Project.all = [];
 
-// Create a handlebars template and populate the project descriptions
-Project.prototype.toHtml = function() {
-  var source = $('#template').html();
-  var template = Handlebars.compile(source);
-  this.daysAgo = parseInt((new Date() - new Date(this.date))/60/60/24/1000);
-  this.publishStatus = this.date ? 'published ' + this.daysAgo + ' days ago' : '(draft)';
-  return template(this);
-};
+  // Create a handlebars template and populate the project descriptions
+  Project.prototype.toHtml = function() {
+    var source = $('#template').html();
+    var template = Handlebars.compile(source);
+    this.daysAgo = parseInt((new Date() - new Date(this.date))/60/60/24/1000);
+    this.publishStatus = this.date ? 'published ' + this.daysAgo + ' days ago' : '(draft)';
+    return template(this);
+  };
 
-// Sort projects by date, with the most recently published projects at the top.
-Project.loadAll = function(dataToSort) {
-  dataToSort.sort(function(a,b) {
-    return (new Date(b.date)) - (new Date(a.date));
-  }).forEach(function(ele) {
-    Project.all.push(new Project(ele));
-  });
-};
-
-// Fetch the data (from localStorage if possible, with an ajax call if not), then call the loadAll and renderIndex functions to display it.
-
-Project.fetchAll = function() {
-  if (localStorage.projectData) {
-    Project.loadAll(JSON.parse(localStorage.getItem('projectData')));
-    sectionView.renderIndex();
-  } else {
-    $.ajax('data/projectData.json', {
-      method: 'GET',
-      success: successHandler,
-      error: errorHandler
+  // Sort projects by date, with the most recently published projects at the top.
+  Project.loadAll = function(dataToSort) {
+    dataToSort.sort(function(a,b) {
+      return (new Date(b.date)) - (new Date(a.date));
+    }).forEach(function(ele) {
+      Project.all.push(new Project(ele));
     });
+  };
 
-    function successHandler(data) {
-      localStorage.setItem('projectData', JSON.stringify(data));
-      Project.loadAll(data);
-      sectionView.renderIndex();
+  // Fetch the data (from localStorage if possible, with an ajax call if not), then call the loadAll and renderIndex functions to display it.
+
+  Project.fetchAll = function(callback) {
+    if (localStorage.projectData) {
+      Project.loadAll(JSON.parse(localStorage.getItem('projectData')));
+      callback();
+    } else {
+      $.ajax('data/projectData.json', {
+        method: 'GET',
+        success: successHandler,
+        error: errorHandler
+      });
+
+      function successHandler(data) {
+        localStorage.setItem('projectData', JSON.stringify(data));
+        Project.loadAll(data);
+        callback();
+      }
+
+      function errorHandler(data) {
+        $('#projects').html('<h3>Error</h3><p> :0 :0 :0 Mistakes were made. :( :( :( </p>');
+      }
     }
 
-    function errorHandler(data) {
-      $('#projects').html('<h3>Error</h3><p> :0 :0 :0 Mistakes were made. :( :( :( </p>');
-    }
-  }
-
-};
+  };
+  module.Project = Project;
+})(window);
