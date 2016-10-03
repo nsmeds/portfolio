3096d2f70c29c8464f365dc1da9491c116e86fc9
nsmeds <niilosmeds@gmail.com>
2016-10-03 14:54:25 -0700

    Minor edits for style/clarity

--- scripts/sectionView.js
+++ scripts/sectionView.js
@@ -14,10 +14,10 @@
   // Show only the first paragraph of each project description, then reveal hidden paragraphs when 'more' is clicked.
   sectionView.showMore = function() {
     $('.project *:nth-of-type(n+3)').hide();
-    $('a.read-more').on('click', function(e) {
+    $('.read-more').on('click', function(e) {
       e.preventDefault();
       var $selectedProject = $(this).parent();
-      if($(this).text() === 'More ...') {
+      if ($(this).text() === 'More ...') {
         $selectedProject.find('p').show();
         $(this).html('Less ...');
       } else {
