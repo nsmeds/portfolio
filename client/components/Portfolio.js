8e257b91629f8bd6ac3880bd264744886dd09b0e
nsmeds <niilosmeds@gmail.com>
2017-06-15 14:20:51 -0700

    button styling and behavior

--- client/components/Portfolio.js
+++ client/components/Portfolio.js
@@ -23,15 +23,15 @@ class Portfolio extends React.Component {
 
     render() {
         return (
-            <div className='main-content'>
-                <div className='project-nav'>
-                    <button name='prev' onClick={this.handleClick} disabled={this.currentIndex > 0 ? false : true}>&lang;</button>
+            <div className='main-content portfolio-projects'>
+                {/*<div className='project-nav'>
                     <h4>Recent Projects</h4>
-                    <button name='next' onClick={this.handleClick} disabled={this.currentIndex < this.props.data.length -1 ? false : true}>&rang;</button>
-                </div>
+                </div>*/}
+                <button className='project-nav-button' name='prev' onClick={this.handleClick} disabled={this.currentIndex > 0 ? false : true}>&lang;</button>
                 <ul>
                     <Projects {...this.state.currentProject}/>
                 </ul>
+                <button className='project-nav-button' name='next' onClick={this.handleClick} disabled={this.currentIndex < this.props.data.length -1 ? false : true}>&rang;</button>
             </div>
         )
     }
