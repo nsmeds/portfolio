16bd22c111cd80ccd59637afc986500c9ac0d453
nsmeds <niilosmeds@gmail.com>
2017-06-21 13:48:57 -0700

    images, styling

--- client/components/Projects.js
+++ client/components/Projects.js
@@ -11,8 +11,8 @@ const Projects = props => {
     return (
                 <li className="project-li">
                     <h4><a href={props.url}>{props.title}</a></h4>
-                    <a href={props.links.github}><img className="project-image" src={props.image} /></a>
                     <div className="project-body" dangerouslySetInnerHTML={createBodyHtml()}></div>
+                    {props.image && <a href={props.links.github}><img className="project-image" src={props.image} /></a>}
                     <div className="project-body">
                         <a href={props.links.github}>Source on Github</a> |
                         <a href={props.links.live}> Live Site</a>
