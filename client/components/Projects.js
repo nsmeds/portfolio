6f2da6d6c08198c09237af1803c5aaa2ee415ded
nsmeds <niilosmeds@gmail.com>
2017-06-21 13:27:00 -0700

    integrate project images

--- client/components/Projects.js
+++ client/components/Projects.js
@@ -9,12 +9,14 @@ const Projects = props => {
     }
 
     return (
-                <li>
+                <li className="project-li">
                     <h4><a href={props.url}>{props.title}</a></h4>
-                    <img src={props.img} />
-                    <div dangerouslySetInnerHTML={createBodyHtml()}></div>
-                    <a href={props.links.github}>Source on Github</a> |
-                    <a href={props.links.live}> Live Site</a>
+                    <a href={props.links.github}><img className="project-image" src={props.image} /></a>
+                    <div className="project-body" dangerouslySetInnerHTML={createBodyHtml()}></div>
+                    <div className="project-body">
+                        <a href={props.links.github}>Source on Github</a> |
+                        <a href={props.links.live}> Live Site</a>
+                    </div>
                 </li>
     )
 }
