1026ecdd6f3d2c6b1d751764276f2cc0b89c447b
nsmeds <niilosmeds@gmail.com>
2017-06-22 16:05:35 -0700

    style, content for about/contact

--- client/components/About.js
+++ client/components/About.js
@@ -3,13 +3,15 @@ import React from 'react';
 const About = props => {
     return (
         <div className="main-content" id="about">
-            <h4>About Me</h4>
-            <p>
-                I'm a software developer based in Portland, Oregon, with a music industry background and a focus on building scalable, reactive web and mobile applications with modern JavaScript. 
-            </p>
-            <p>
-                I'm currently working as a freelance contractor and I'm interested in finding a full-time position with a team creating exciting, meaningful work.
-            </p>
+            <div className="content-wrap">
+                <h4>About Me</h4>
+                <p>
+                    I'm a software developer based in Portland, Oregon, with a music industry background and a focus on building scalable, reactive web and mobile applications with modern JavaScript. 
+                </p>
+                <p>
+                    I'm currently working as a freelance contractor and I'm interested in finding a full-time position with a team creating exciting, meaningful work.
+                </p>
+            </div>
         </div>
     )
 }
--- client/components/Contact.js
+++ client/components/Contact.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import Mailto from 'react-mailto';
 
 const Contact = props => {
     return (
         <div className="main-content" id="contact">
-            <h4>Contact Me</h4>
-            <p>You can find me on Github, LinkedIn or email me.</p>
+            <div className="content-wrap">
+                <h4>Contact Me</h4>
+                <p>You can find me on <a href="https://github.com/nsmeds">Github</a>, <a href="https://www.linkedin.com/in/niilo-smeds-4a0605120">LinkedIn</a> or <Mailto email="niilosmeds@gmail.com" obfuscate={true}>email me</Mailto>.</p>
+            </div>
         </div>
     )
 }
